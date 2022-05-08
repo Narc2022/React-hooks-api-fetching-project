0e32bfb014beeaa1ca9a97ac210b97621c55207d
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2022-05-08 06:39:04 +0530

    2nd commit

---
+++ api_youtube/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import UseEffectAPI from './Component/UseEffectAPI.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <UseEffectAPI />
+    </div>
+  );
+}
+
+export default App;
---
+++ api_youtube/src/Component/UseEffectAPI.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const UseEffectAPI = () => {
+  return (
+    <>
+        <h1>List of Github User</h1>
+        <div className="container-fluid mt-5">
+          <div className="row text-center">
+            <div className="col-10 col-md-4 mt-5"></div>
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default UseEffectAPI;
